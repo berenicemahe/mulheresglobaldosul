668cf1fc7a4887654429dff4c203c40ef16c4695
Renata Grassi <renagrassi@gmail.com>
2020-10-02 12:00:26 -0300

    implemented carousel at about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :contact]
 
   def home
   end
 
   def about
+    
   end
 
   def contact
