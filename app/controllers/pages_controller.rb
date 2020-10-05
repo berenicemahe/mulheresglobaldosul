ded78815c26accf273e14c30471f24ecd5c7184f
Renata Grassi <renagrassi@gmail.com>
2020-10-05 18:34:31 -0300

    working on about us page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about, :contact]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :contact, :historia]
 
   def home
   end
 
   def about
-    
   end
 
   def contact
+  end
+
+  def historia
   end
 end
