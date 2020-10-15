7980a67d927c784d6a29ed2349e8d98276c823c3
Berenice Mahe <berenice.mahe@gmail.com>
2020-10-15 16:26:46 -0300

    product controller done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,26 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
   def edit
     @product = Product.find(params[:id])
   end
@@ -8,12 +29,17 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
 
     if @product.update(product_params)
-      redirect_to product_path(@product.id)
+      redirect_to products_path
     else
       render :edit
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products, only: [:edit, :update]
+  resources :products
   get 'contacts/new'
 
   get 'messages/new'
