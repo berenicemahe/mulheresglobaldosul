478cfb4699631afcf96907e2926d83ff7ddbb0bc
Berenice Mahe <berenice.mahe@gmail.com>
2020-10-15 10:37:18 -0300

    first step of contato form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'contacts/new'
-  # post 'contacts/create'
-  # get 'contacts/create'
-  # resources :contacts, only: [ :new, :create ]
-  # get 'contacts/thanks'
+
+  get 'messages/new'
+  post 'messages/create'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
