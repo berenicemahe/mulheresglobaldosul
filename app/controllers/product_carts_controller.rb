aa07d79ad35329249c6237d72d8b33c847eeb4e6
Renata Grassi <renagrassi@gmail.com>
2020-10-15 18:20:29 -0300

    making routes and controller of product_carts

---
+++ app/controllers/product_carts_controller.rb
@@ -0,0 +1,15 @@
+class ProductCartsController < ApplicationController
+  def index
+    @product_carts = ProductCart.all
+  end
+
+  
+ 
+  private
+
+  def select_produtc_cart
+    @product_cart = ProductCart.select(current_user)
+  end
+
+
+end
