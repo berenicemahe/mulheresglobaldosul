93bebba70b4c9f1ea02012f6ceeeadf9e778ed1e
Bianca Ferreira <biancaferreiralp@gmail.com>
2020-10-16 12:04:08 -0300

    Implemented routes to Carts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
+    devise_for :users
+  root to: 'pages#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :products, only: [:edit, :update]
+
   resources :product_carts, only: [:index, :create]
+
+  resources :carts, only: [:index, :show]
+
   get 'contacts/new'
 
   get 'messages/new'
   post 'messages/create'
-
-  devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
