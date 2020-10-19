fa5488d4f2e31ec64c8d70e4666c77186dca9094
Berenice Mahe <berenice.mahe@gmail.com>
2020-10-19 14:58:58 -0300

    seeds started

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'contacts/create'
   # resources :contacts, only: [ :new, :create ]
   # get 'contacts/thanks'
+  resources :products
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
