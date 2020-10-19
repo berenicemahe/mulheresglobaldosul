83918ecbe2df9b1b4c2a24c69d9b47a899bdb229
Renata Grassi <renagrassi@gmail.com>
2020-10-19 11:23:40 -0300

    starting to work on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,30 @@
+require 'faker'
+
+puts "Starting creating products..."
+
+p1 = Product.new(name: 'T-shirt', price: 50,
+  color: "verde", size: 'G', gender: 'Masculino', type: 'blusas e camisetas', description: 'Green t-shirt with flowers')
+p1.save!
+
+p2 = Product.new(name: 'skirt', price: 30,
+  color: "cinza", size: 'M', gender: 'Feminino', type: 'saias', description: 'Gray long skirt')
+p2.save!
+
+p3 = Product.new(name: 'dress', price: 100,
+color: "vermelho", size: 'P', gender: 'Feminino', type: 'vestidos e macacões', description: 'Summer dress')
+p3.save!
+
+puts 'Products were created'
+
+
+
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
