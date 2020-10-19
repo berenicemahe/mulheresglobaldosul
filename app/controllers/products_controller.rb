cc9b3ada774e201b8f95ba46145d60ad536e7068
Berenice Mahe <berenice.mahe@gmail.com>
2020-10-19 11:43:17 -0300

    product controller and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,26 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
   def edit
     @product = Product.find(params[:id])
   end
@@ -14,6 +35,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy
+    redirect_to products_path
+  end
 
   private
 
