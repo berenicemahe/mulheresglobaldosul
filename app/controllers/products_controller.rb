ab87290ae6a0fe614551fed4fcfdaca118087764
Berenice Mahe <berenice.mahe@gmail.com>
2020-11-13 10:08:32 -0300

    some changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,7 +44,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :gender, :description, :size, :type, :color, :price)
+    params.require(:product).permit(:name, :gender, :description, :size, :type, :color, :price, :photo)
   end
 
 end
